55387f1bf6e558f3af82c59dfb39787698ab9991
em3rald1 <morozdatat@gmail.com>
2025-01-31 23:15:51 +0100

    ??

--- modules/pinkspaces-nixos/hypr/default.nix
+++ modules/pinkspaces-nixos/hypr/default.nix
@@ -2,5 +2,7 @@
   programs.hyprland.enable = true;
   programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   homix.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  homix.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
+  homix.".config/hypr/wallpapers".source = ./wallpapers;
   environment.systemPackages = with pkgs; [ kitty ];
 }
